42a4682a2b2d53e8ec5f713cc915b49c1ebe0681
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-08 23:22:58 -0500

    Third Commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 //     quantity: 0,
 //     isPurchased: false
 //   }
-//Make an array of 3 objects using the above shape and render the item, quantity and units (12 pack, 1lb, 2 liters, etc.)
+//1. Make an array of 3 objects using the above shape and render the item, quantity and units (12 pack, 1lb, 2 liters, etc.)
 
 const groceryList = [
     {
@@ -32,16 +32,17 @@ const groceryList = [
 ]
 console.table(groceryList)
 
-//Make inputs so that new items can be added
+
 class App extends React.Component {
     state = {
+        grocery: groceryList,
         item: "",
         brand: "",
         units: "",
         quantity: 0,
         isPurchased: false
     }
-
+    // Every time we do something (type) inside the input, handleChange will be invoked
     handleChange = (event) => {
         this.setState({
             [event.target.id]: event.target.value
@@ -57,9 +58,11 @@ class App extends React.Component {
             quantity: this.state.isPurchased
         }
         console.log(newListItem)
-
+       
+        // Correct way of updating
+        // event.target.id allows us to use the id we specified in the form to make our handleChange dynamic
         this.setState({
-            products: [newListItem, ...this.state.products],
+            grocery: [newListItem, ...this.state.grocery],
             item: "",
             brand: "",
             units: "",
@@ -72,24 +75,35 @@ class App extends React.Component {
                 <h1> Grocery List</h1>
 
                 <h2> Insert</h2>
+                {/* 2. Make inputs so that new items can be added */}
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="item"> Item: </label>
                     <input id="item" type="text" value={this.state.item} onChange={this.handleChange} />
                     <label htmlFor="brand"> Brand: </label>
-                    <input id="brand" type="text" value={this.state.item} onChange={this.handleChange} />
+                    <input id="brand" type="text" value={this.state.brand} onChange={this.handleChange} />
                     <label htmlFor="units"> Units: </label>
-                    <input id="units" type="text" value={this.state.item} onChange={this.handleChange} />
+                    <input id="units" type="text" value={this.state.units} onChange={this.handleChange} />
                     <label htmlFor="quantity"> Quantity: </label>
-                    <input id="quantity" type="text" value={this.state.item} onChange={this.handleChange} />
+                    <input id="quantity" type="text" value={this.state.quantity} onChange={this.handleChange} />
                 </form>
 
                 <div>
-                    <h2>Preview our new item</h2>
+                    <h2>These Items Have Been Added to Your Grocery List</h2>
                     <h3>{this.state.item}</h3>
                     <h4>{this.state.brand}</h4>
                     <h5>{this.state.units}</h5>
                     <h5>{this.state.quantity}</h5>
                 </div>
+                {/* 3. Conditionally render the grocery items based on whether or not they were purchased (ok to have hard coded values for isPurchased) */}
+                <ul>
+                    {
+                        this.state.grocery.map(purchased =>{
+                            return(
+                                <li>{purchased.item}{purchased.isPurchased}</li>
+                            )
+                        })
+                    }
+                </ul>
 
             </div>
         )
